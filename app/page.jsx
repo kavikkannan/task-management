26f4959d82a0855e1bab85bfe6eea8117d647e57
kavikkannan <kavikkannan.k@gmail.com>
2023-10-26 22:54:20 +0530

    26-10-22:30

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import Header_LPage from '../components/Header_LPage';
-
+import Body_Lpage from '@/components/Body_Lpage';
 
 export default function Home() {
   return (
     <div> 
       <Header_LPage/>
+      <Body_Lpage/>
     </div>
   );
 }
---
+++ components/Body_Lpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body_Lpage = () => {
+  return (
+    <div>Body_Lpage</div>
+  )
+}
+
+export default Body_Lpage
